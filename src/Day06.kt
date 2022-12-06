ac7dc9365cbf806fa2e9eedb84d6696a987d5cdd
Alexandr Jeliuc <alex@jeliuc.com>
2022-12-06 10:24:57 +0200

    Day 06 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,22 +1,27 @@
 fun main() {
+    fun findStartOfMessageMarker(stringSequence: String, size: Int): Int {
+        stringSequence.toCharArray().toList().windowed(size).forEachIndexed { k, v ->
+            if (v.toSet().size == size) return k + size
+        }
+
+        throw Exception("Marker not found")
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return findStartOfMessageMarker(input.first(), 4)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return findStartOfMessageMarker(input.first(), 14)
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
-    println("Test 1 - ${part1(testInput)}")
-    println("Test 2 - ${part2(testInput)}")
+    check(part1(testInput) == 7)
 
     val input = readInput("Day06")
     println("Part 1 - ${part1(input)}")
     println("Part 2 - ${part2(input)}")
-    check(part1(input) == 0)
-    check(part2(input) == 0)
+    check(part1(input) == 1896)
+    check(part2(input) == 3452)
 }
 
