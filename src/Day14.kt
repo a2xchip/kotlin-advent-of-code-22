78f053738a8dbf6cbddf5b59f1d99f1c696240a7
Alexandr Jeliuc <alex@jeliuc.com>
2022-12-16 03:23:54 +0200

    Day 14 template

---
+++ src/Day14.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day14_test")
+    println("Test 1 - ${part1(testInput)}")
+    println("Test 2 - ${part2(testInput)}")
+    println()
+    val input = readInput("Day14")
+    println("Part 1 - ${part1(input)}")
+    println("Part 2 - ${part2(input)}")
+}
